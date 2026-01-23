2dc218e217154af47587746a576c0598c1e4b6ef
AndreVasques <andrevasques@outlook.com.br>
2026-01-22 22:34:20 -0300

    feat(client): implement client controller methods, implement client response DTO
    add Spring Actuator for observability

--- src/main/java/br/andre/financialsystem/controller/ClientController.java
+++ src/main/java/br/andre/financialsystem/controller/ClientController.java
@@ -1,9 +1,45 @@
 package br.andre.financialsystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.andre.financialsystem.domain.model.Client;
+import br.andre.financialsystem.dto.client.ClientResponse;
+import br.andre.financialsystem.dto.client.CreateClientRequest;
+import br.andre.financialsystem.service.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    private final ClientService service;
+
+    public ClientController(ClientService clientService) {
+        this.service = clientService;
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(ClientService.class);
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> save(@RequestBody CreateClientRequest request) {
+        log.info("POST_CLIENT_REQUEST creating client with cpf={}", request.getCpf());
+        Client client = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ClientResponse(client));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponse> findById(@PathVariable String id) {
+        log.info("GET_CLIENT_RESPONSE finding client with id={}", id);
+        Client client = service.findById(id);
+        return ResponseEntity.ok(new ClientResponse(client));
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<ClientResponse> findByCpf(@PathVariable String cpf) {
+        log.info("GET_CLIENT_RESPONSE finding client with cpf={}", cpf);
+        Client client = service.findByCpf(cpf);
+        return ResponseEntity.ok(new ClientResponse(client));
+    }
 }
--- src/main/java/br/andre/financialsystem/dto/client/ClientResponse.java
+++ src/main/java/br/andre/financialsystem/dto/client/ClientResponse.java
@@ -1,10 +1,34 @@
 package br.andre.financialsystem.dto.client;
 
 import br.andre.financialsystem.domain.enums.ClientStatus;
+import br.andre.financialsystem.domain.model.Client;
 
 public class ClientResponse {
-    private String id;
-    private String name;
-    private String email;
-    private ClientStatus status;
+    private final String id;
+    private final String name;
+    private final String email;
+    private final ClientStatus status;
+
+    public ClientResponse(Client client) {
+        this.id = client.getId();
+        this.name = client.getName();
+        this.email = client.getEmail();
+        this.status = client.getStatus();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ClientStatus getStatus() {
+        return status;
+    }
 }
