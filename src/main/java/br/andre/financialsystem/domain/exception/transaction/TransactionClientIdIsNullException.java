0c17909d3d79789c8ff4de74048777dc4f0f634c
AndreVasques <andrevasques@outlook.com.br>
2026-01-23 15:40:59 -0300

    refactor(handler):  - add new exception handlers

--- src/main/java/br/andre/financialsystem/domain/exception/transaction/TransactionClientIdIsNullException.java
+++
@@ -1,7 +0,0 @@
-package br.andre.financialsystem.domain.exception.transaction;
-
-public class TransactionClientIdIsNullException extends RuntimeException {
-    public TransactionClientIdIsNullException() {
-        super("transaction client id is null");
-    }
-}
--- src/main/java/br/andre/financialsystem/handler/ApiExceptionHandler.java
+++ src/main/java/br/andre/financialsystem/handler/ApiExceptionHandler.java
@@ -1,8 +1,8 @@
 package br.andre.financialsystem.handler;
 
-import br.andre.financialsystem.domain.exception.client.ClientNotFoundException;
-import br.andre.financialsystem.domain.exception.client.CpfAlreadyExistsException;
-import br.andre.financialsystem.domain.exception.client.InvalidCpfException;
+import br.andre.financialsystem.domain.exception.client.*;
+import br.andre.financialsystem.domain.exception.transaction.InvalidTransactionValueException;
+import br.andre.financialsystem.domain.exception.transaction.TransactionNotFoundException;
 import br.andre.financialsystem.dto.handler.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,5 +28,29 @@ public class ApiExceptionHandler {
     public ResponseEntity<ApiError> handle(InvalidCpfException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ApiError("INVALID_CPF", "CPF is invalid"));
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ApiError> handle(InsufficientBalanceException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ApiError("INSUFFICIENT_BALANCE", "Insufficient balance"));
+    }
+
+    @ExceptionHandler(ClientNotActiveException.class)
+    public ResponseEntity<ApiError> handle(ClientNotActiveException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ApiError("CLIENT_NOT_ACTIVE", "Client not active"));
+    }
+
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<ApiError> handle(TransactionNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ApiError("TRANSACTION_NOT_FOUND", "Transaction not found"));
+    }
+
+    @ExceptionHandler(InvalidTransactionValueException.class)
+    public ResponseEntity<ApiError> handle(InvalidTransactionValueException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError("INVALID_TRANSACTION_VALUE", "Invalid transaction value"));
     }
 }
