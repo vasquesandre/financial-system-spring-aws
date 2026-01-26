909684a80f445f9710ac290b37556b737b22addc
AndreVasques <andrevasques@outlook.com.br>
2026-01-26 11:30:38 -0300

    test(client):  - create client controller tests

---
+++ src/test/java/br/andre/financialsystem/controller/ClientControllerTest.java
@@ -0,0 +1,119 @@
+package br.andre.financialsystem.controller;
+
+import br.andre.financialsystem.domain.enums.ClientStatus;
+import br.andre.financialsystem.domain.model.Client;
+import br.andre.financialsystem.dto.client.CreateClientRequest;
+import br.andre.financialsystem.service.ClientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class ClientControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ClientService clientService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldCreateClient() throws Exception {
+        CreateClientRequest request = new CreateClientRequest(
+                "Andre",
+                "12345678900",
+                "123456",
+                "andre@email.com"
+        );
+
+        Client client = new Client(
+                "1",
+                request.getName(),
+                request.getCpf(),
+                request.getPassword(),
+                request.getEmail(),
+                BigDecimal.ZERO,
+                ClientStatus.ACTIVE,
+                Instant.now()
+        );
+
+        when(clientService.create(any())).thenReturn(client);
+
+        mockMvc.perform(post("/clients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Andre"))
+                .andExpect(jsonPath("$.balance").value(0))
+                .andExpect(jsonPath("$.email").value("andre@email.com"))
+                .andExpect(jsonPath("$.status").value(ClientStatus.ACTIVE.toString()));
+
+    }
+
+    @Test
+    void shouldFindClientById() throws Exception {
+        Client client = new Client(
+                "1",
+                "Andre",
+                "12345678900",
+                "123456",
+                "andre@email.com",
+                BigDecimal.ZERO,
+                ClientStatus.ACTIVE,
+                Instant.now()
+        );
+
+        when(clientService.findById("1")).thenReturn(client);
+
+        mockMvc.perform(get("/clients/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Andre"))
+                .andExpect(jsonPath("$.balance").value(0))
+                .andExpect(jsonPath("$.email").value("andre@email.com"))
+                .andExpect(jsonPath("$.status").value(ClientStatus.ACTIVE.toString()));
+    }
+
+    @Test
+    void shouldFindClientByCpf() throws Exception {
+        Client client = new Client(
+                "1",
+                "Andre",
+                "12345678900",
+                "123456",
+                "andre@email.com",
+                BigDecimal.ZERO,
+                ClientStatus.ACTIVE,
+                Instant.now()
+        );
+
+        when(clientService.findByCpf("12345678900")).thenReturn(client);
+
+        mockMvc.perform(get("/clients/cpf/12345678900"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Andre"))
+                .andExpect(jsonPath("$.balance").value(0))
+                .andExpect(jsonPath("$.email").value("andre@email.com"))
+                .andExpect(jsonPath("$.status").value(ClientStatus.ACTIVE.toString()));
+    }
+}
